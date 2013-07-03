c1d9491e970b4600eea7b2bce8e07cc7a92d494b
Eric Schles <ericschles@gmail.com>
2013-07-03 08:54:22 -0700

    exercise 6

---
+++ ex6.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+use 5.010;
+use strict;
+use utf8;
+
+say "Now we are going to learn about concatenation";
+say "Hello"."world";
+say "hello" . ' ' . "world";
+print 'hello world' . "\n";
+
